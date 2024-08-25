ad175209c8065f4f87e556ac418f27e41f2e5bec
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-08-25 16:27:59 +0300

    add FragmentListBinding

--- app/src/main/java/com/example/p13_depi_android_task/ListFragment.kt
+++ app/src/main/java/com/example/p13_depi_android_task/ListFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.p13_depi_android_task.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
+    private lateinit var binding: FragmentListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_fromListToAdd)
+        }
     }
 
 }
