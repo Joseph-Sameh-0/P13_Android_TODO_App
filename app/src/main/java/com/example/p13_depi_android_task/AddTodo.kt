e342c8dfee5a19fe666851096101af701f2e5203
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-08-25 19:12:28 +0300

    Edit AddTodo Fragment to add FloatingActionButton

--- app/src/main/java/com/example/p13_depi_android_task/AddTodo.kt
+++ app/src/main/java/com/example/p13_depi_android_task/AddTodo.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.graphics.Rect
+import android.view.ViewTreeObserver
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddTodo : Fragment() {
     override fun onCreateView(
@@ -15,4 +20,25 @@ class AddTodo : Fragment() {
         return inflater.inflate(R.layout.fragment_add_todo, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rootLayout = view.findViewById<RelativeLayout>(R.id.root_layout)
+        val fab = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        rootLayout.viewTreeObserver.addOnGlobalLayoutListener {
+            val r = Rect()
+            rootLayout.getWindowVisibleDisplayFrame(r)
+            val screenHeight = rootLayout.rootView.height
+            val keypadHeight = screenHeight - r.bottom
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // Keyboard is opened
+                fab.animate().translationY(-keypadHeight.toFloat()).setDuration(300).start()
+            } else {
+                // Keyboard is closed
+                fab.animate().translationY(0f).setDuration(300).start()
+            }
+        }
+    }
 }
