49f54d0ad5f09eed03b267e03ace34b59104c2fd
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-08-26 04:05:05 +0300

    make generated Id in TODO data class

--- app/src/main/java/com/example/p13_depi_android_task/TODO.kt
+++ app/src/main/java/com/example/p13_depi_android_task/TODO.kt
@@ -1,7 +1,18 @@
 package com.example.p13_depi_android_task
 
 data class TODO(
-    val id:Int,
-    val title:String,
-    val description:String
-)
+    val id: Int = generateId(),
+    val title: String,
+    val description: String
+){
+    companion object {
+        private var currentId: Int = 0
+
+        private fun generateId(): Int {
+            return ++currentId
+        }
+    }
+}
+
+
+
